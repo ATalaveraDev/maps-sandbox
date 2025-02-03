947b31f3f1176fcd0c4accd5a5c076e9918cab7e
Angel Talavera <atalavera86@gmail.com>
2025-02-03 19:59:30 +0100

    Implemented HERE maps

--- src/app/components/here-maps/here-maps.component.ts
+++ src/app/components/here-maps/here-maps.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
+import '@here/maps-api-for-javascript';
 
 @Component({
   selector: 'app-here-maps',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
   styleUrl: './here-maps.component.css',
   standalone: true
 })
-export class HereMapsComponent {
+export class HereMapsComponent implements AfterViewInit {
+  private map?: H.Map;
 
+  @ViewChild('map') mapDiv?: ElementRef;
+  
+  ngAfterViewInit(): void {
+    if (!this.map && this.mapDiv) {
+      const platform = new H.service.Platform({
+        apikey: ''
+      });
+      const layers = platform.createDefaultLayers();
+      this.map = new H.Map(
+        this.mapDiv.nativeElement,
+        (layers as any).vector.normal.map,
+        {
+          pixelRatio: window.devicePixelRatio,
+          center: {lat: 52.5, lng: 13.4},
+          zoom: 13,
+        }
+      );
+    }
+  }
 }
